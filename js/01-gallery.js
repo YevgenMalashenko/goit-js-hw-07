49e24f091ce3efe56de2a89113dc1a701777420f
YevgenMalashenko <malashenkoyevgen1986@gmail.com>
2022-08-04 22:14:49 +0300

    выполняет пункт 1

---
+++ js/01-gallery.js
@@ -0,0 +1,31 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryElement = document.querySelector(".gallery");
+
+const itemsToInsert = galleryItems
+  .map(
+    (item) =>
+      `<li class = "gallery__item"><a class = "gallery__link" href = "${item.original}"><img class = "gallery__image"src = "${item.preview}"data-source="${item.original}"alt = "${item.description}"></li>`
+  )
+  .join("");
+
+galleryElement.insertAdjacentHTML("beforeend", itemsToInsert);
+
+galleryElement.addEventListener("click", onImageHandler);
+
+function onImageHandler(e) {
+  e.preventDefault();
+  if (e.target.className !== "gallery__image") {
+    return;
+  }
+
+  const highResolutionImageUrl = galleryItems.find(
+    (object) => object.preview === e.target.src
+  ).original;
+
+  const instance = basicLightbox.create(`
+  <img src="${highResolutionImageUrl}">
+`);
+
+  instance.show();
+}
