21a222ef0f702f1ead35a7db9deeacb7d6da2a2e
YevgenMalashenko <malashenkoyevgen1986@gmail.com>
2022-08-05 13:30:01 +0300

    makes hw

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const galleryElement = document.querySelector(".gallery");
+
+const itemsToInsert = galleryItems
+  .map(
+    (item) =>
+      `<li class = "gallery__item"><a class = "gallery__link" href = "${item.original}"><img class = "gallery__image"src = "${item.preview}"alt = "${item.description}"></li>`
+  )
+  .join("");
+
+galleryElement.insertAdjacentHTML("beforeend", itemsToInsert);
+
+const lisagsadafsghtbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
